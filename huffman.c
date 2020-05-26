e54ab8798d69606effdac8cbc1a674c0004025d5
Tom Raybould <Thomaseraybould@gmail.com>
2020-05-26 19:14:26 -0400

    Print statements commented out

--- huffman.c
+++ huffman.c
@@ -16,7 +16,6 @@ long 				read_in_file		(char *file_name, unsigned char **file_contents);
 void 				scale_freqs			(int *freqs);
 FILE*               write_to_file 		(char *filename, unsigned char* compressed_data, int total_bytes);
 void	 			build_tree          (const int *freqs, BiTree** tree);
-void 				print_int_as_bi		(const int num);
 void 				decompress_file		(char *file_name);
 void 				print_char_arr		(unsigned char *char_arr, int length);
 
@@ -46,7 +45,7 @@ long read_in_file(char *fileName, unsigned char **file_contents){
 	unsigned char *buffer;
 	long file_len;
 
-	printf("%s\n", fileName);
+	//printf("%s\n", fileName);
 	file = fopen(fileName, "rb");  	// Open the file in binary mode
 	fseek(file, 0, SEEK_END);      	// Jump to the end of the file
 	file_len = ftell(file);     	// Get the current byte offset in the file
@@ -87,7 +86,7 @@ void compress(char *file_name){
 
 	unsigned char *buffer = *file_contents;
 
-	print_char_arr(buffer, (int) file_len);
+	//print_char_arr(buffer, (int) file_len);
 
 	int freqs[CHAR_MAX + 1];
 
@@ -110,7 +109,7 @@ void compress(char *file_name){
 
 	HashMap *map = buildHashMap(tree);
 
-	HashMap_print(map);
+	//HashMap_print(map);
 
 	int header_size = sizeof(int) + (CHAR_MAX + 1);
 
@@ -125,7 +124,7 @@ void compress(char *file_name){
 	unsigned int input_pos 	= 0;
 	unsigned int output_pos = (unsigned int) (header_size * 8);
 
-    printf("Compression: \n");
+    //printf("Compression: \n");
 	for(input_pos = 0; input_pos < total_chars; input_pos++){
 
 		HuffmanMapData *huffmanMapData;
@@ -133,7 +132,7 @@ void compress(char *file_name){
 		HashMap_get(map, buffer[input_pos], (void **) &huffmanMapData);
 
 		for(int i = 0; i < huffmanMapData -> bit_length; i++){
-			printf("bit_length %d\n", huffmanMapData -> bit_length);
+			//printf("bit_length %d\n", huffmanMapData -> bit_length);
 			if(output_pos % 8 == 0){
 				compressed = realloc(compressed, (output_pos / 8) + 1);
 			}
@@ -153,9 +152,9 @@ void compress(char *file_name){
 
 	}
 
-	print_char_arr(compressed, (output_pos / 8 + 1));
+	//print_char_arr(compressed, (output_pos / 8 + 1));
 
-    printf("\n");
+    //printf("\n");
 	write_to_file(NULL, compressed, (output_pos / 8 + 1));
 
 	free(buffer); // Close the file
@@ -236,8 +235,8 @@ void build_tree(const int *freqs, BiTree **tree){
     	occurrence -> value  = (unsigned char) c;
     	occurrence -> weight = freqs[c];
 
-    	print_occurrence(occurrence);
-    	printf("\n");
+    	//print_occurrence(occurrence);
+    	//printf("\n");
 
         BiTree *temp_tree = malloc(sizeof(BiTree));
         BiTree_init(temp_tree, destroy_occurrence, print_occurrence);
@@ -248,9 +247,9 @@ void build_tree(const int *freqs, BiTree **tree){
 
     }
 
-    printf("%s", "\n");
+    //printf("%s", "\n");
 	//Heap_print(heap);
-	printf("\n");
+	//printf("\n");
 
 	while(heap -> size > 1){
 		BiTree *tree1, *tree2;
@@ -287,7 +286,7 @@ void build_tree(const int *freqs, BiTree **tree){
 
 	Heap_pop(heap, (void **) tree);
 
-	BiTree_level_order_print(*tree);
+	//BiTree_level_order_print(*tree);
 
 }
 
@@ -300,19 +299,19 @@ void recsMapPop(HashMap *map, BiTreeNode *node, unsigned int code_bit_length, un
 	//only for leaf nodes
 	if(code_bit_length > 0 && node -> right == NULL && node -> left == NULL){
 
-        printf("code %d\n", code);
+        //printf("code %d\n", code);
 		HuffmanMapData *data    = malloc(sizeof(HuffmanMapData));
 		data -> code	        = htonl(code);
 		data -> bit_length      = code_bit_length;
 
 		Occurrence *occurrence = (Occurrence*) node -> data;
 
-		printf("key: %c, code: %d, bit_length: %d \n", occurrence -> value,data -> code, data -> bit_length);
+		//printf("key: %c, code: %d, bit_length: %d \n", occurrence -> value,data -> code, data -> bit_length);
 
 		HashMap_put(map, occurrence -> value, data);
 	}
 	else{
-	    printf("bit length: %d\n", code_bit_length);
+	    //printf("bit length: %d\n", code_bit_length);
 	}
 
 	code = code << 1;
@@ -335,7 +334,7 @@ void HuffmanMapData_print(void* data){
 
 HashMap* buildHashMap(BiTree *tree){
 
-    BiTree_level_order_print(tree);
+    //BiTree_level_order_print(tree);
 
     void (*destroy_data)(void*) = free_data;
 
@@ -343,7 +342,7 @@ HashMap* buildHashMap(BiTree *tree){
 
 	recsMapPop(map, tree -> root, 0, 0x0000);
 
-	HashMap_print(map);
+	//HashMap_print(map);
 
 	return map;
 
@@ -371,13 +370,13 @@ void decompress_file(char *file_name){
 
 	long file_len = read_in_file(file_name, file_contents);
 
-	printf("%ld\n", file_len);
+	//printf("%ld\n", file_len);
 
 	unsigned char *buffer = *file_contents;
 
 	int original_file_length = *((int*) buffer);
 
-	print_char_arr(buffer, (int)file_len);
+	//print_char_arr(buffer, (int)file_len);
 
 	int freqs[CHAR_MAX + 1];
 
@@ -394,7 +393,7 @@ void decompress_file(char *file_name){
 	unsigned int processed_bytes = 0;
 	unsigned char uncompressed_data[original_file_length];
 
-	printf("og file size%d\n", original_file_length);
+	//printf("og file size%d\n", original_file_length);
 
 	for(unsigned int i = header_size; i < file_len; i++){
 		if(processed_bytes == original_file_length){
@@ -410,12 +409,12 @@ void decompress_file(char *file_name){
 					break;
 				}
 				curr_node = tree -> root;
-				print_occurrence(occurrence);
-				printf("%s\n", "");
+				//print_occurrence(occurrence);
+				//printf("%s\n", "");
 			}
 
 			int bit = bit_get(buffer, (8 * i) + j);
-			printf(" bit  %d\n", bit);
+			//printf(" bit  %d\n", bit);
 			
 			if(bit){
 				curr_node = curr_node -> right;
@@ -427,7 +426,7 @@ void decompress_file(char *file_name){
 		}
 	}
 
-	print_char_arr(uncompressed_data, original_file_length);
+	//print_char_arr(uncompressed_data, original_file_length);
 }
 
 void parse_compressed_freqs(unsigned char* buffer, int* freqs){
@@ -471,19 +470,6 @@ void printBits(char * data, size_t size_of){
 
 	printf("\n");
 
-}
-
-
-void print_int_as_bi(const int num){
-
-	for (int j = 0; j < 32; ++j) {
-		if(j % 4 == 0 && j != 0){
-			printf(" ");
-		}
-		printf("%d", bit_get((const unsigned char *) &num, j));
-	}
-
-	printf("\n");
 }
 
 void print_char_arr(unsigned char *char_arr, int length){
